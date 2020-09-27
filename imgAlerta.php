2301e0c8e09a7600e83068c04ba3f26a5d978f9e
unknown <martin22sala@gmail.com>
2020-09-27 14:30:15 -0300

    practica con imagen

---
+++ imgAlerta.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
+
+    <title>Img</title>
+</head>
+<body>
+        <h3>Resultado</h3>
+<?php   
+$numero = $_POST['numero'];
+
+if($numero == 4){
+    echo 'Perfecto!!',"<img src=ok.png>";
+        }else{
+            echo 'NO!',"<img src=error.png>";
+        }
+?>
+
+<div>
+    <a href="enviaImg.html" class="">volver</a>
+    </div>
+    
+</body>
+</html>
+
+
+
